9daae77093b0b6044f55e3a2dd535b68b288a037
mao <Mstarfish@163.com>
2018-04-16 18:32:09 -0400

    Questions Answered

--- q.py
+++ q.py
@@ -1,20 +1,40 @@
-def deleteDuplicated(list):
-        resultList = []
-        for item in list:
-                if not item in resultList:
-                        resultList.append(item)
-        return resultList
+# ''' 
+# Python File for Leetcode Question Schedule Follow Up
+# Version: 0.1 
+# '''
+
+import sys
+from os import listdir
+from os.path import splitext
+
+global q
+
+def finished(dir):
+	finished = []
+	for i in listdir('./'+dir):
+		i_temp = splitext(i)[0]
+		if i_temp.isdigit():
+			finished.append(int(i_temp))
+
+	print dir+':'
+	print ' finished: ', len(finished)
+	if (sorted( set(q) - set(finished) ) == 0):
+		print "All finished, Congratulation!!!\n"
+	else:
+		print ' unfinished: ', sorted( set(q) - set(finished) )
+
+	return finished
 
 # week 1
 q = [1, 136, 3, 771, 535, 217, 149, 202, 138, 49, 463, 219, 760, 36, 85]
 q += [2, 237, 21, 206, 23, 148, 141, 138, 328, 234, 160, 147, 142, 24, 19]
 
-q = deleteDuplicated(q)
+q = list(set(q))
 q.sort()
 
-# Finished
-m_finished = []
+print 'Total_Questions: ', len(set(q))
 
-print 'Mao unfinished: ', sorted( set(q) - set(m_finished) )
-print 'Total_len: ', len(set(q))
+# Finished
+m_finished = finished('Mao')
+s_finished = finished('shadow')
 
