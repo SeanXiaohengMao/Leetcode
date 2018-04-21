b8ae99fc53f2edbe05970c53567f9f3f80860b75
Zhidong Liu <zl2591@columbia.edu>
2018-04-21 17:52:07 -0400

    Questions Answered

---
+++ shadow/19.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+
+// one pointer
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        int length = 0;
+
+        while (head != null) {
+            length++;
+            head = head.next;
+        }
+
+        int remain = length - n;
+        head = dummy;
+        while (remain > 0) {
+            head = head.next;
+        }
+
+        head.next = head.next.next;
+        return dummy.next;
+    }
+}
+
+// two pointers
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode first = dummy;
+        ListNode second = dummy;
+
+        for (int i = 0; i < n + 1; i++) {
+            first = first.next;
+        }
+
+        while (first != null) {
+            first = first.next;
+            second = second.next;
+        }
+
+        second.next = second.next.next;
+        return dummy.next;
+    }
+}
