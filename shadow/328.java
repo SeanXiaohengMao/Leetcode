5a7843b69f5db692b4ced1b2c913f8cdbe7bd0fd
Zhidong Liu <zl2591@columbia.edu>
2018-04-21 23:03:08 -0400

    Questions Answered

---
+++ shadow/328.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenh = even;
+
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            even.next = even.next.next;
+            odd = odd.next;
+            even = even.next;
+        }
+
+        odd.next = evenh;
+        return head;
+    }
+}
