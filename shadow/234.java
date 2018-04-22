fa49640fd6826c00717f163852e63758455134fe
Zhidong Liu <zl2591@columbia.edu>
2018-04-21 22:36:44 -0400

    Questions Answered

---
+++ shadow/234.java
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null) {
+            return true;
+        }
+
+        ListNode mid = findMid(head);
+        ListNode right = reverse(mid.next);
+        mid.next = null;
+
+        while (right != null) {
+            if (head.val != right.val) {
+                return false;
+            }
+            head = head.next;
+            right = right.next;
+        }
+
+        return true;
+    }
+
+    private ListNode findMid(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode temp = head.next;
+            head.next = prev;
+            prev = head;
+            head = temp;
+        }
+
+        return prev;
+    }
+
+}
