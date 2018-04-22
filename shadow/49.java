4d9a78be7c991f5c20c1998c67bde43eeb643429
Zhidong Liu <zl2591@columbia.edu>
2018-04-22 00:31:59 -0400

    Questions Answered

---
+++ shadow/49.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return new ArrayList<>();
+        }
+
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] ca = str.toCharArray();
+            Arrays.sort(ca);
+            String key = String.valueOf(ca);
+
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(str);
+        }
+
+        return new ArrayList(map.values());
+    }
+}
